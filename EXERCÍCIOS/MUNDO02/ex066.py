d674700ac64afddaeb58e1202ced94f6d95d53f7
alexklenio <alexlopes.see@gmail.com>
2025-09-08 21:37:16 -0300

    exercicios

---
+++ EXERCÍCIOS/MUNDO02/ex066.py
@@ -0,0 +1,13 @@
+soma = 0
+cont = 0
+
+while True:
+    numero = int(input("Digite o número 999 para parar: "))
+
+    if numero == 999:
+        break
+    else:
+        soma += numero
+        cont+=1
+
+print(f"\nVocê informou {cont} números e a soma de todos eles é igual a {soma}")
