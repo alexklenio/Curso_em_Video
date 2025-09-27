26a4aff589a2528fa9d17b1558029eeddacedca5
alexklenio <alexlopes.see@gmail.com>
2025-09-27 18:52:56 -0300

    exercicio 90

---
+++ EXERCÍCIOS/MUNDO03/ex090.py
@@ -0,0 +1,15 @@
+aluno = dict()
+
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif aluno['media'] >= 5 and aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado' 
+
+print("-=" * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
