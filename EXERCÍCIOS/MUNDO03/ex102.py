dfd316d77dc3b9fa62026f44ce130b56da354432
alexklenio <alexlopes.see@gmail.com>
2025-09-27 18:37:06 -0300

    Update ex102.py

--- EXERCÍCIOS/MUNDO03/ex102.py
+++ EXERCÍCIOS/MUNDO03/ex102.py
@@ -7,8 +7,10 @@ def fatorial(n,show = False):
     """
     f = 1
     for c in range (n, 0, -1):
+
         if show:
             print(c, end='')
+
             if c>1:
                 print(' x ', end='')
             else:
@@ -21,4 +23,3 @@ n = int(input('\nInforme o numero o qual você quer ver o fatorial: '))
 print()
 print(fatorial(n, show=True))
 
-#help(fatorial)
