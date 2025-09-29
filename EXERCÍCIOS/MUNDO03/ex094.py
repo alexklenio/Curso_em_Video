2ac3b813b089a02d75396f310e6d0b2d246fdef0
alexklenio <alexlopes.see@gmail.com>
2025-09-28 21:15:35 -0300

    ex094

---
+++ EXERCÍCIOS/MUNDO03/ex094.py
@@ -0,0 +1,44 @@
+galera = list()
+pessoa = dict()
+soma = media = 0
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M / F] ')).upper().strip()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+
+    while True:
+        resp = str(input('Quer Continuar? [S / N] ')).upper().strip()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break   
+
+print('-=' * 30)
+print(f'AO todo temos {len(galera)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'A média de idade é de {media:5.2f} anos.')
+print('As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print('Lista de pessoas acima da média: ', end='')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+
+print('<< ENCERRADO >>')
