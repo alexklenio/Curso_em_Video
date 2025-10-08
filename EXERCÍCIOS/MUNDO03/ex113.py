bb4a01db928e5d348c3ce8c76885e858b75c0dd3
alexklenio <alexlopes.see@gmail.com>
2025-10-08 19:14:13 -0300

    ex113

---
+++ EXERCÍCIOS/MUNDO03/ex113.py
@@ -0,0 +1,31 @@
+def leia_int(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO:por favor, digite um numero inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[31mO usuário preferiu não digitar esse número.\033[m')
+            return 0
+            break
+        else:
+            return n
+        
+def leia_float(msg):
+    while  True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+                print('\033[31mERRO:por favor, digite umn[umero inteiro válido.\033[m')
+                continue
+        except (KeyboardInterrupt):
+                print('\033[31mO usuário preferiu não digitar esse número.\033[m')
+                return 0
+        else:
+                return n
+
+
+n1 = leia_int('Digite um valor: ')
+n2 = leia_float('Digite um número Real: ')
+print(f'O número inteiro digitado foi {n1} e o real foi {n2}')
