52a33affb4e994c300185ed12400722cde9345d8
alexklenio <alexlopes.see@gmail.com>
2025-10-09 21:41:45 -0300

    ex115b

--- EXERCÍCIOS/MUNDO03/ex115/sistema.py
+++ EXERCÍCIOS/MUNDO03/ex115/sistema.py
@@ -1,14 +1,24 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = "cursoemvideo.txt"
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
-    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    resposta = menu(
+        ["Ver pessoas cadastradas", "Cadastrar nova Pessoa", "Sair do Sistema"]
+    )
     if resposta == 1:
-       cabecalho('Opção 1')
+        cabecalho("Opção 1")
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        cabecalho("Opção 2")
     elif resposta == 3:
-        cabecalho('Saindo do sistema... Até logo!')
+        cabecalho("Saindo do sistema... Até logo!")
         break
     else:
-        print('\033[31mERRO! Sigite uma opção válida\033[m')
+        print("\033[31mERRO! Sigite uma opção válida\033[m")
         sleep(2)
